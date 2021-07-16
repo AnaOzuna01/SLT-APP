337ad1001e033064fda100ce6cb03c3113ec674a
Ana Ozuna <anamol2699@hotmail.com>
2021-07-15 23:33:59 -0400

    Color format detection & color TTS

--- app/src/main/java/com/finalproject/sltapplication/MainActivity.java
+++ app/src/main/java/com/finalproject/sltapplication/MainActivity.java
@@ -9,17 +9,22 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
@@ -38,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
     TextView txtCoordinates;
     TextView txtColor;
     TextToSpeech textToSpeech;
+
+    double x = -1;
+    double y = -1;
 
     private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -86,6 +94,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void onPause() {
         super.onPause();
+
+        if (cameraBridgeViewBase != null){
+            cameraBridgeViewBase.disableView();
+        }
+        if (textToSpeech != null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
     }
 
     public void onResume(){
@@ -120,6 +136,60 @@ public class MainActivity extends AppCompatActivity {
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputCameraFrame){
         matRGBA = inputCameraFrame.rgba();
         return matRGBA;
+    }
+
+    public boolean onDisplay(View view, MotionEvent motionEvent){
+
+        int rows = matRGBA.rows();
+        int cols = matRGBA.cols();
+
+        double yHigh = (double) cameraBridgeViewBase.getHeight() * 0.7696078;
+        double yLow = (double) cameraBridgeViewBase.getHeight() * 0.2401961;
+        double yScale = (double) rows / (yHigh - yLow);
+        double xScale = (double) cols / (double) cameraBridgeViewBase.getWidth();
+
+        x = motionEvent.getX();
+        y = motionEvent.getY();
+
+        y = y - yLow;
+
+        x = x * xScale;
+        y = y * yScale;
+
+        if((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
+
+        txtCoordinates.setText("X: " + Double.valueOf(x) + ", Y: " + Double.valueOf(y));
+
+        Rect rectDisplay = new Rect();
+
+        rectDisplay.x = (int) x;
+        rectDisplay.y = (int) y;
+
+        rectDisplay.width = 8;
+        rectDisplay.height = 8;
+
+        Mat touchedRegRGBA = matRGBA.submat(rectDisplay);
+        Mat touchedRegHSV = new Mat();
+
+        Imgproc.cvtColor(touchedRegRGBA, touchedRegHSV, Imgproc.COLOR_RGB2HSV_FULL);
+
+        colorHSV = Core.sumElems(touchedRegHSV);
+        int point = rectDisplay.width * rectDisplay.height;
+        for (int i = 0; i < colorHSV.val.length; i++){
+            colorHSV.val[i] /= point;
+        }
+
+        colorRGBA = fromHSVtoRGBA(colorHSV);
+
+        String colorsName = color.getRGBName( (int) colorRGBA.val[0], (int) colorRGBA.val[1], (int) colorRGBA.val[2]);
+        txtColor.setText("Color = #" + String.format("%02X", (int) colorRGBA.val[0]) + String.format("%02X", (int) colorRGBA.val[1]) +
+                String.format("%02X", (int) colorRGBA.val[2]));
+        txtColor.setTextColor(android.graphics.Color.rgb((int) colorRGBA.val[0], (int) colorRGBA.val[1],
+                (int) colorRGBA.val[2]));
+
+        Toast.makeText(getApplicationContext(), colorsName, Toast.LENGTH_SHORT).show();
+        textToSpeech.speak(colorsName, TextToSpeech.QUEUE_FLUSH, null);
+        return false;
     }
 
     public Scalar fromHSVtoRGBA(Scalar colorHSV){
